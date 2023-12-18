0081cbca2813f0a0986255ddae9289dfa63d4236
NB367\Jimmy <jimmyw@jimmythedeveloper.com>
2023-12-18 12:37:36 +0800

    data function must return an object data defines which data will be used in the VueJS app mount defines which part of the DOM will be manipulated by the Vue
    v-model establish a connection between the input and the value in the data property (it listens to the value change and automatically update the value)
    v-on is used to add event listener (e.g. v-on:click)
    v-for is used to replicate html element (e.g. <li v-for="goal in goals">{{ goal }}</li>)

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,30 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+// Imperative approach
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement("li");
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = "";
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement("li");
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = "";
+// }
 
-buttonEl.addEventListener("click", addGoal);
+// buttonEl.addEventListener("click", addGoal);
+
+Vue.createApp({
+  //to define which data will be used in the VueJS App
+  //data function must return an object
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+    },
+  },
+}).mount("#app"); //css selector here # = id, . = class;
