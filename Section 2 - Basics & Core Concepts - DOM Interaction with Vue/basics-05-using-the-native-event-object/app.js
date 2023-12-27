a1ea6e1547e36bea4418fdb3e48355bb0a233d19
NB367\Jimmy <jimmyw@jimmythedeveloper.com>
2023-12-27 17:03:40 +0800

    1. any non-event bound method wil be re-executed by Vue whenever anything on the screen changes (because Vue can't know what the method does), and this will affect the performance

--- Section 2 - Basics & Core Concepts - DOM Interaction with Vue/basics-05-using-the-native-event-object/app.js
+++ Section 2 - Basics & Core Concepts - DOM Interaction with Vue/basics-05-using-the-native-event-object/app.js
@@ -6,6 +6,13 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    outputFullname() {
+      console.log("Running again...");
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Wan";
+    },
     setName(event, lastName) {
       this.name = event.target.value;
     },
