ec3106ffb203dc8b8e86c9da99bd65268e5b0a01
NB367\Jimmy <jimmyw@jimmythedeveloper.com>
2023-12-27 18:14:46 +0800

    1. introduced watchers with "watched" property, it is similar to "computed" property 2. when to use "computed": when you just want to calculate some output value dynamically 3. when to use "watch": when you wanna run some code, which may be, but not necessarily also update data property in reaction to a property changing

--- Section 2 - Basics & Core Concepts - DOM Interaction with Vue/basics-05-using-the-native-event-object/app.js
+++ Section 2 - Basics & Core Concepts - DOM Interaction with Vue/basics-05-using-the-native-event-object/app.js
@@ -3,16 +3,47 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
+      fullname: "",
     };
   },
-  computed: {
-    fullname() {
-      console.log("Running again...");
-      if (this.name === "") {
-        return "";
+  watch: {
+    //you can use the same name that is defined in the data function, or in the computed property (e.g. counter, name, fullname)
+    //if this.name changes, the following watcher method will re-execute
+    //the "value" parameter here is refer to "this.name"
+    name(value) {
+      if (value === "") {
+        this.fullname = "";
+      } else {
+        this.fullname = value + " " + "Wan";
       }
-      return this.name + " " + "Wan";
     },
+    lastName(value) {
+      if (value === "") {
+        this.fullname = "";
+      } else {
+        this.fullname = this.name + " " + value;
+      }
+    },
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 1000);
+      }
+    },
+  },
+
+  computed: {
+    // Commenting out the fullname computed property
+    // fullname() {
+    //   console.log("Running again...");
+    //   if (this.name === "") {
+    //     return "";
+    //   }
+    //   return this.name + " " + "Wan";
+    // },
   },
   methods: {
     setName(event, lastName) {
