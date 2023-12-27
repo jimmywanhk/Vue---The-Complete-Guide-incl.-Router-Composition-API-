c1c15816aafeb7d487c28230cd040355b59ce659
NB367\Jimmy <jimmyw@jimmythedeveloper.com>
2023-12-27 13:57:20 +0800

    1. introduced v-bind, v-bind is used to set the value of an attribute on an HTML element 2. data is a function that must return an object 3. methods is an object which is full of methods 4. {{ }} you can call function in double curly braces syntax

--- Section 2 - Basics & Core Concepts - DOM Interaction with Vue/basics-01-starting-code/app.js
+++ Section 2 - Basics & Core Concepts - DOM Interaction with Vue/basics-01-starting-code/app.js
@@ -1,8 +1,21 @@
 const app = Vue.createApp({
+  //data is a function to return an object
   data() {
     return {
       courseGoal: "Finish the course and learn Vue!", //you can output anything here, e.g. string, integer, array and object
+      vueLink: "https://vuejs.org/",
     };
+  },
+  //methods is an object which is full of methods
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
   },
 });
 
