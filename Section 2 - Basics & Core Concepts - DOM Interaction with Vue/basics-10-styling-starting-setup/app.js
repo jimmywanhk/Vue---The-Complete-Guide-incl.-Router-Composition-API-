1cf53981ba403a8eccf8533ab8d0e73ddf07545d
DESKTOP\Administrator <wankaho@hotmail.com>
2024-01-01 22:31:07 +0800

    1. adding css classes dynamically (refactor code) 2. add toggle functionality

--- Section 2 - Basics & Core Concepts - DOM Interaction with Vue/basics-10-styling-starting-setup/app.js
+++ Section 2 - Basics & Core Concepts - DOM Interaction with Vue/basics-10-styling-starting-setup/app.js
@@ -8,9 +8,9 @@ const app = Vue.createApp({
   },
   methods: {
     boxSelected(box) {
-      if (box === "A") this.boxASelected = true;
-      else if (box === "B") this.boxBSelected = true;
-      else if (box === "C") this.boxCSelected = true;
+      if (box === "A") this.boxASelected = !this.boxASelected;
+      else if (box === "B") this.boxBSelected = !this.boxBSelected;
+      else if (box === "C") this.boxCSelected = !this.boxCSelected;
     },
   },
 });
